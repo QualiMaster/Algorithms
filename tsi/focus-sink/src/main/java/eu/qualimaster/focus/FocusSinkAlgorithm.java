e6a2c94006911566fc74c069778607fe37097799
Apostolos Nydriotis <anydriotis@gmail.com>
2016-11-01 17:21:02 +0200

    fix focus sink algorithm (writer had been removed...)

--- tsi/focus-sink/src/main/java/eu/qualimaster/focus/FocusSinkAlgorithm.java
+++ tsi/focus-sink/src/main/java/eu/qualimaster/focus/FocusSinkAlgorithm.java
@@ -1,11 +1,9 @@
 package eu.qualimaster.focus;
 
-import eu.qualimaster.common.logging.DataLogger;
 import eu.qualimaster.data.inf.IFocusSink;
 import eu.qualimaster.dataManagement.strategies.IStorageStrategyDescriptor;
 import eu.qualimaster.observables.IObservable;
 
-//import org.apache.log4j.LogManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,19 +13,20 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Properties;
 
+//import org.apache.log4j.LogManager;
+
 /**
  * Created by ap0n on 10/2/2016.
  */
 public class FocusSinkAlgorithm implements IFocusSink {
 
   private static final String PROPERTIES_PATH = "/var/nfs/qm/tsi/external-service.properties";
-  //    private static final String CORRELATION_RESULT_SERVER_IP = "snf-618466.vm.okeanos.grnet.gr";
-  //  private static final String CORRELATION_RESULT_SERVER_IP = "147.27.14.117";
   private static String CORRELATION_RESULT_SERVER_IP = "clu01.softnet.tuc.gr";
   private static int CORRELATION_RESULT_SERVER_PORT = 8888;
   private Logger logger = LoggerFactory.getLogger(FocusSinkAlgorithm.class);
   private Socket socket;
   private boolean terminated;
+  private PrintWriter writer;
 
   public FocusSinkAlgorithm() {
     this.terminated = false;
@@ -45,8 +44,6 @@ public class FocusSinkAlgorithm implements IFocusSink {
         .equals("")) {
       return;
     }
-//    LogManager.getLogger(getClass()).info("Got: " + data.toString());
-
     StringBuilder sb = new StringBuilder();
     sb.append("focusPip,");
     if (ticket != -1) {
@@ -59,7 +56,9 @@ public class FocusSinkAlgorithm implements IFocusSink {
 
   @Override
   public void postDataEdgeStream(IFocusSinkEdgeStreamInput data) {
-    if (terminated) return;
+    if (terminated) {
+      return;
+    }
     emit(-1, data);
   }
 
@@ -91,14 +90,14 @@ public class FocusSinkAlgorithm implements IFocusSink {
 
   private void sendStr(String str) {
     try {
-//      writer.println(str);
-//      writer.flush();
+      writer.println(str);
+      writer.flush();
     } catch (Exception e) {
       logger.error("Not connected to results server. Reconnecting...");
       try {
         connect();
-//        writer.println(str);
-//        writer.flush();
+        writer.println(str);
+        writer.flush();
       } catch (Exception ex) {
         logger.error("Can't connect to results server.");
         logger.error(ex.getMessage(), ex);
@@ -115,13 +114,15 @@ public class FocusSinkAlgorithm implements IFocusSink {
       CORRELATION_RESULT_SERVER_IP = properties.getProperty("IP");
       CORRELATION_RESULT_SERVER_PORT = Integer.parseInt(properties.getProperty("PORT"));
     } catch (IOException ioex) {
-      System.err.println(ioex.getMessage());
-      ioex.printStackTrace();
+      logger.error(ioex.getMessage(), ioex);
     } finally {
       if (inputStream != null) {
         try {
           inputStream.close();
         } catch (IOException ex) {
+          logger.warn(
+              PROPERTIES_PATH + " not found! Using default IP: " + CORRELATION_RESULT_SERVER_IP
+              + " PORT: " + CORRELATION_RESULT_SERVER_PORT);
           // Ignore exception, means file not found or something similar. Fall back to defaults.
           //          ex.printStackTrace();
         }
@@ -132,7 +133,7 @@ public class FocusSinkAlgorithm implements IFocusSink {
   @Override
   public void disconnect() {
     terminated = true;
-//    writer.close();
+    writer.close();
     try {
       socket.close();
     } catch (IOException e) {
@@ -156,7 +157,10 @@ public class FocusSinkAlgorithm implements IFocusSink {
   }
 
   private void connectToResultsServer() throws IOException {
+    logger.info("Connecting to results server");
     socket = new Socket(CORRELATION_RESULT_SERVER_IP, CORRELATION_RESULT_SERVER_PORT);
-//    writer = new PrintWriter(socket.getOutputStream());
+    logger.info("Connected. IP: " + socket.getInetAddress().getHostAddress()
+                + " PORT:" + socket.getPort());
+    writer = new PrintWriter(socket.getOutputStream());
   }
 }
